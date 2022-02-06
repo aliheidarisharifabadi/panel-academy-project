0e2749a2b4e33bd5e6096b695af0c0070134debf
aliheidari <ali70heidari@chmail.ir>
2022-02-06 17:28:59 +0330

    read category list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -9,30 +9,31 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title> پیشخوان </title>
-    <link rel="stylesheet" href="style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }} " />
 </head>
 
 <body>
+<?php
+$categories = \App\Models\Category::all();
+?>
 <div class="wrapper">
     <div class="header"></div>
     <div class="right">
-        <h3 style="padding: 5px;">مدیریت وبلاگ</h3>
-        <a href="index.php?insert_post"> درج پست جدید</a>
-        <a href="index.php?view_posts"> مشاهده همه پست ها</a>
-        <a href="index.php?insert_cat"> درج دسته بندی جدید</a>
-        <a href="index.php?view_cats"> مشاهده همه دسته بندی ها</a>
-        <a href="index.php?menu_new"> درج منو جدید</a>
-        <a href="logout.php">خروج</a>
+        <h3 style="padding: 5px;">panel</h3>
+@foreach($categories as $category)
+    <a href="?cat_{{$category->id}}"> {{$category->title}}</a>
 
+@endforeach
     </div>
     <div class="left">
         <h2 style="color:#c33;"><?php echo @$_GET['logged'];?></h2>
         <span style="font-size:18px;">خوش آمدید</span><h2 style="color:#03c;"><?php // echo $_SESSION['user_name'];
             ?></h2>
+
+        @if(isset($_GET['cat_1']))
+          {{34}}
+        @endif
         <?php
-        if(isset($_GET['insert_post'])){
-          //  require_once('insert_post.php');
-        }
         if(isset($_GET['view_posts'])){
           //  require_once('../includes/view_posts_post.php');
         }
